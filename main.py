434e5d721bf0901f964dc5da44df9270958f3ed4
Chai <83523587+CPScript@users.noreply.github.com>
2022-11-30 14:52:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pyfiglet
+from termcolor import colored
 
 ascii_banner = pyfiglet.figlet_format("SpiltWine")
 print(ascii_banner)
 
-
-
+text = colored("Find any open ports on a IP address", "blue")
+print(text)
 
 
 import socket
@@ -21,8 +22,8 @@ def check_ip(address):
 
 def scan(target):
     ip_address = check_ip(target)
-    print(f"\n[-_0 Scanning Target] {str(target)}")
-    for port in range(1, 100):
+    print(f"\n-_0 looking for all open Ports on IP:{str(target)}")
+    for port in range(1, 688029):
         scan_port(ip_address, port)
 
 
@@ -36,7 +37,7 @@ def scan_port(ipaddress_, port_):
         pass
 
 
-targets = input("[+] Enter Target(s) to scan: ")
+targets = input("[+] Enter IP address to scan: ")
 
 if ',' in targets:
     for target in targets.split(','):
@@ -44,3 +45,41 @@ if ',' in targets:
 
 else:
     scan(targets)
+import time
+print("Would you like to get premeium?")
+time.sleep(2)
+print("Stuff that comes with premium!")
+time.sleep(5)
+print("[+]Faster connection?")
+time.sleep(1)
+print("[+]IP geoLocater?")
+time.sleep(1)
+print("[+]DDOS attack script?")
+time.sleep(1)
+print("[+]VPN")
+time.sleep(1)
+print("[+]Protection service ")
+time.sleep(1)
+print("[+]Phishing tool for other APPLICATIONS")
+time.sleep(1)
+print("[+]AD blocker")
+time.sleep(1)
+print("[+]Black listed on our public Servers and IP pullers")
+time.sleep(1)
+print("[+]RAT tools")
+print("[+]Free malwares that we have made.")
+print(" ")
+print(" ")
+time.sleep(3)
+text = colored("Price started at 30$ a month or 250$ a year!", "blue")
+print(text)
+print(" ")
+print(" ")
+print ("Yes or No")
+choice = input("")
+
+if choice == "yes":
+    print("Please message this user on discord: Fe4RLess#0001")
+
+if choice == "no":
+  print("-_- OK -_-")
